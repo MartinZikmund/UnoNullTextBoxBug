bca3351f259ecb454b2b1e2d9a792298c1e1d002
Martin Zikmund <martinzikmund@live.com>
2019-07-25 17:32:29 +0200

    Add project files.

---
+++ UnoNullTextBox.Shared/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UnoNullTextBox.Shared;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoNullTextBox
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        
+        public TestItem Test { get; private set; } = new TestItem();
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Test = new TestItem();
+            OnPropertyChanged(nameof(Test));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ UnoNullTextBox.Shared/TestItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnoNullTextBox.Shared
+{
+    public class TestItem
+    {
+        public string Text { get; set; } 
+    }
+}
